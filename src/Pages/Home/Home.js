2f7f63b8fca82b0e5354fd8d5fd9a56853f6a801
STÉFANI LEMOS FERREIRA <stefani_l_ferreira@estudante.sesisenai.org.br>
2024-05-02 21:54:24 -0300

    home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,34 +5,24 @@ import imageHome from "../../imagens/imageHome.png";
 const Home  = () => {
     return(
 
-
-    <div className="ContainerHome">
-
+    <body className="bodyhome">
         <section className="sectionHome">
-            
-        <div className="imageHome">
-                <img src={imageHome} className="imagehome"></img>
-        </div>
-        <div className="formHome">
-            <div>
-                <h1>Olá</h1>
-                <h1>Será bem vindo de volta.</h1>
+            <div className="containerHome">
+                <div className="imageHome">
+                    <article className="FormHome">
+                        <div className="Formulario">
+                            <h1 className="title1">Olá</h1>
+                            <h1 className="title1">Seja bem vindo de volta!</h1>
+                        </div>
+                    </article>
+                    
+                </div>
+               
             </div>
-
-            <div>
-                <h1>Faça o seu login!</h1>
-                <input placeholder="E-mail"></input>
-                <input placeholder="Senha"></input>
-
-                <h1>Esqueceu sua senha? Clique Aqui</h1>
-
-                <button>Login</button>
-             </div>
-        </div>
-
+    
         </section>
-
-    </div>
+        
+    </body>
 
     )
 }
