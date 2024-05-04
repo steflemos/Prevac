5663fd50f98197265d5be618df54b6b515ac1ab8
STÉFANI LEMOS FERREIRA <stefani_l_ferreira@estudante.sesisenai.org.br>
2024-05-03 21:26:23 -0300

    home check

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,25 +4,36 @@ import imageHome from "../../imagens/imageHome.png";
 
 const Home  = () => {
     return(
-
-    <body className="bodyhome">
-        <section className="sectionHome">
-            <div className="containerHome">
-                <div className="imageHome">
-                    <article className="FormHome">
-                        <div className="Formulario">
-                            <h1 className="title1">Olá</h1>
-                            <h1 className="title1">Seja bem vindo de volta!</h1>
+<body className="bodyhome">
+    <section className="sectionHome">
+        <div className="containerHome">
+            <div className="imageHome">
+                <article className="FormHome">
+                    <div className="Formulario">
+                        <div className="TitulosHome">
+                        <h1 className="title1">Olá!</h1>
+                        <h1 className="title1">Seja bem vindo de volta.</h1>
                         </div>
-                    </article>
-                    
-                </div>
-               
+                        
+                        <p className="pLogin">Faça o seu login</p>
+                        <div className="containerInputs">
+                        <input placeholder="E-mail" className="InputFormHome"></input>
+                        <input placeholder="Senha" type="password" className="InputFormHome"></input>
+                        <p className="pLogin2">Esqueceu a sua senha? <a href="#" className="linkHome">Clique Aqui</a></p>
+                        </div>
+                        <div className="DivButton">
+                            <button className="buttonLogin">Login</button>
+                        </div>
+                    </div>
+                </article>
+                
             </div>
+           
+        </div>
+
+    </section>
     
-        </section>
-        
-    </body>
+</body>
 
     )
 }
