c4171b1bd82be5d67bed4328856abd7bfc585820
Stefani Lemos Ferreira <94566412+steflemos@users.noreply.github.com>
2024-05-29 12:51:36 -0300

    renomeando itens

--- src/Pages/Home/components/cards/Cards.js
+++ src/Pages/Home/components/cards/Cards.js
@@ -2,34 +2,34 @@ import Icon1 from "../../images/icon1.png";
 import "./cards.css"
 
 function Cards() {
-    return (
-        <>
-        <div className="ContainerSection">
+ return (
+  <>
+  <div className="CardContainer">
 
-       
-       <section class="secao-cartoes">
-        <div class="cartao">
-        <img src={Icon1} alt="icon"  className="imgIconCard" />
-            <h2 className="TitleCard">Título do Cartão 1</h2>
-            <p className="TextCards">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et risus vitae libero varius fermentum.</p>
-            
-        </div>
-        <div class="cartaoCentro">
-        <img src={Icon1} alt="icon"  className="imgIconCard" />
-            <h2 className="TitleCard">Título do Cartão 2</h2>
-            <p className="TextCards">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et risus vitae libero varius fermentum.</p>
-            
-        </div>
-        <div class="cartao">
-        <img src={Icon1} alt="icon"  className="imgIconCard" />
-            <h2 className="TitleCard">Título do Cartão 3</h2>
-            <p className="TextCards">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et risus vitae libero varius fermentum.</p>
-            
-        </div>
-    </section>
-    </div>
-        </>
-    );
+ 
+ <section className="CardSection">
+  <div className="Card">
+  <img src={Icon1} alt="icon" className="CardIcon" />
+   <h2 className="CardTitle">Card Title 1</h2>
+   <p className="CardText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et risus vitae libero varius fermentum.</p>
+   
+  </div>
+  <div className="Card CenterCard">
+  <img src={Icon1} alt="icon" className="CardIcon" />
+   <h2 className="CardTitle">Card Title 2</h2>
+   <p className="CardText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et risus vitae libero varius fermentum.</p>
+   
+  </div>
+  <div className="Card">
+  <img src={Icon1} alt="icon" className="CardIcon" />
+   <h2 className="CardTitle">Card Title 3</h2>
+   <p className="CardText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et risus vitae libero varius fermentum.</p>
+   
+  </div>
+ </section>
+ </div>
+  </>
+ );
 }
 
 export default Cards;
